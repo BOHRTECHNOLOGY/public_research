b40080f1550a036c0db59cd524a8c83c313e1d02
zackgow <zackgow@gmail.com>
2019-01-19 17:54:23 -0500

    add class for Quantum Kitchen Sinks

--- Experiments/forest_QKS/2019_01_08_initial_implementation/qks.py
+++ Experiments/forest_QKS/2019_01_08_initial_implementation/qks.py
@@ -1,239 +1,207 @@
+"""Quantum Kitchen Sinks
+"""
+
 import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.utils import shuffle
-
-from pyquil import get_qc
 from pyquil.quil import Program
 from pyquil.gates import RX, CNOT, MEASURE
 
+class QuantumKitchenSinks():
+    """Quantum Kitchen Sinks (QKS)
 
-def make_frames(num):
-    """Generates picture frames dataset.
-    
-    The dataset contains a smaller inner square
-    with sides of length 2 and a larger outer square with sides
-    of length 4. Both have points uniformally distributed around
-    the average.
-    """
-    def zip_arrays(d1, d2):
-        return np.array(list(zip(d1, d2)))
+    Uses quantum circuits to nonlinearly transform classical inputs into random features.
 
-    def make_squares(num, l):
-        left_right, top_bottom = make_barriers(num, l), make_barriers(num, l)
-        top_bottom[:,[0,1]] = top_bottom[:,[1,0]] 
-        square = np.concatenate((left_right, top_bottom))
-        return square
+    Parameters
+    ----------
+    qc : object, default = None
+        the quantum computer object
 
-    def make_barriers(num, l):
-        dist = 0.1 * l
-        left = np.random.uniform((-l - dist), (-l + dist), num)
-        right = np.random.uniform((l - dist), (l + dist), num)
-        left_barrier = np.random.uniform(-l, l, num)
-        right_barrier = np.random.uniform(-l, l, num)
-        L = zip_arrays(left, left_barrier)
-        R = zip_arrays(right, right_barrier)
-        barriers = np.vstack((L, R))
-        return barriers
-    
-    outer = make_squares(num, l=2)
-    inner = make_squares(num, l=1)
-    frames = np.concatenate((outer, inner))
-    
-    half = int((len(frames) / 2))
-    target = np.concatenate((np.zeros(half), np.ones(half)))
-    
-    X, y = shuffle(frames, target, random_state=1337)
-    return X, y
+    n_episodes : int, default = 1
+        number of episodes the dataset is iterated over;
+        (i.e. for each sampe in the dataset, we apply the QKS transformation n_episodes number of times)
+
+    p : int, default = None
+        dimension of input vector, inferred from dataset
+
+    q : int, default = None
+        number of qubits to use
+
+    r : int, default = 1
+        number of elements that are non-zero s.t. r <= p (dimension of input vector)
+
+    scale : int, default = 1
+        standard deviation (spread) of the normal distribution
+
+    distribution : string, default = 'normal'
+        distribution used to create nonzero elements of omega
+
+    n_trials : int, default 1000
+        the number of times we run the quantujm circuit
 
 
-def make_omega(n_episodes, p, q, r, scale, loc=0):
-    """A (q x p) dimensional matrix that is used to encode the input vector into q gate parameters.
-    
-    n_episodes: number of episodes the dataset is iterated over
-    q: number of qubits to use
-    p: dimension of input vector
-    r: number of elements that are non-zero s.t. r <= p
-    scale: standard deviation (spread) of the normal distribution
-    loc: mean of the normal distribution
+    References
+    ----------
+    Quantum Kitchen Sinks was introduced in:
+        C. M. Wilson, J. S. Otterbach, N. Tezak, R. S. Smith,
+        G. E. Crooks, and M. P. da Silva 2018. Quantum Kitchen
+        Sinks. An algorithm for machine learning on near-term
+        quantum computers. <https://arxiv.org/pdf/1806.08321.pdf>
+
     """
 
-    def create_selection_matrix(p, q, r):
-        """Generates a matrix of 0s and 1s to zero out `r` values per matrix"""
+    def __init__(self, qc=None, n_episodes=1, p=None, q=None, r=1,
+                 scale=1, distribution='normal', n_trials=1000):
+        self.qc = qc
+        self.n_episodes = n_episodes
+        self.p = p 
+        self.q = q or len(self.qc.qubits())
+        self.r = r
+        self.scale = scale
+        self.distribution = distribution
+        self.n_trials = n_trials
+        self.num_cols = self.n_episodes*self.q
+        self.executable = self._build_and_compile()
         
-        matrix_size = p * q
-        m = np.zeros(matrix_size)
-        for i in range(r):
-            m[i] = 1
-        np.random.shuffle(m)
-        selection_matrix = m.reshape(q, p)
-        return selection_matrix
-
-    normal_dist = np.random.normal(loc, scale, size=(n_episodes, q, p))
-    selection_matrix = np.array([create_selection_matrix(p, q, r) for x in range(n_episodes)])
-    omega = normal_dist * selection_matrix # matrix chooses which values to keep
-    return omega
-
-
-def get_beta(n_episodes, q):
-    """random q-dimensional bias vector
-    
-    n_episodes: number of episodes the dataset is iterated over
-    q: number of qubits to use
-    """
-    return np.random.uniform(low=0, high=(2 * np.pi), size=(n_episodes, q))
-
-
-def get_theta(omega, u, beta, n_episodes):
-    """A linear transformation to get our set of random parameters to feed into the quantum circuit
-    
-    omega: a (q x p) dimensional matrix that is used to encode the input vector into q gate parameters
-    u: p-dimensional input vector from the dataset 
-    beta: random q-dimensional bias vector
-    n_episodes: number of episodes the dataset is iterated over
-    """
-    theta = [omega[e].dot(u[i].T) + beta[e]
-             for i in range(u.shape[0]) 
-             for e in range(n_episodes)
-             ]
-    return np.array(theta)
-
-
-def make_qc(q, noisy=False):
-    q_str = str(q) + 'q'
-    qvm = '-qvm'
-    name = q_str + qvm if not noisy else q_str + '-noisy' + qvm
-    print("name of qc: ", name)
-    qc = get_qc(name)
-    return qc
-
-
-def ansatz(qc):
-    """Creates the quantum circuit and performs the measurements.
-    
-    It mimics the circuits from the appendix of the paper, with the exception of
-    the ordering of the CNOT gates (before and after compilation they still do not match).
-    This is probably best just left up to the compiler. 
-    
-    For ease of reading the printed operations, the qubits are looped over several times.
-    """    
-    program = Program()
-    qubits = qc.qubits()
-    n_qubits = len(qubits)
-    var = 'theta'
-
-    ro = program.declare('ro', memory_type='BIT', memory_size=n_qubits)
-    thetas = {var + str(qubit): program.declare(var + str(qubit), memory_type = 'REAL') for qubit in qubits}
-
-    sq = int(np.sqrt(n_qubits))
-    lim = n_qubits - sq - 1 
-    
-    for m in qubits:
-        program += RX(thetas[var + str(m)], m)
+    def fit(self, X):
+        """Generate set of random parameters for X
         
-    for m in qubits:
-        m_1 = m + 1
-        m_sq = m + sq
-        skip = (m_1) % sq
+        Parameters
+        ---------
+        X : array, shape (n_samples, n_features)
+            Training data
+        """
+
+        self.shape = X.shape
+        self.p = self.shape[-1]
+
+        if not hasattr(self, 'omega'):
+            self.omega = self._make_omega()
+            self.beta = self._make_beta()
+
+        self.theta = self._get_theta(X) 
         
-        if m_1 < n_qubits:
-            if (m_sq >= n_qubits): program += CNOT(m, m_1)
-            else: program += CNOT(m, m_sq)
+    def transform(self):
+        """Apply the QKS transformation to the random parameters"""
+
+        if not hasattr(self, 'theta'):
+            raise NotFittedError("Model has not been fitted yet.")
+
+        transformations = []
+        for theta in self.theta:
+            avg_measurements = self._qks_run(theta)
+            transformations.append(avg_measurements)
+        return np.array(transformations).reshape(self.shape[0], self.num_cols)
     
-        if (m < lim) and (skip != 0): program += CNOT(m, m_1)
+    def fit_transform(self, X):
+        """Generate set of random parameters for X and apply the QKS transformation
 
-    for m in qubits:
-        program += MEASURE(m, ro[m])
+        Parameters
+        ---------
+        X : array, shape (n_samples, n_features)
+            Training data
+        """
 
-    print("program instructions from pyquil:\n")
-    for instruction in program.instructions:
-        print(instruction) 
+        self.fit(X)
+        return self.transform()
+        
+    def _make_omega(self):
+        """A (q x p) dimensional matrix that is used to encode the input vector into q gate parameters."""
 
-    return program
+        def _create_selection_matrix():
+            """Generates a matrix of 0s and 1s to zero out `r` values per matrix"""
 
+            matrix_size = self.p * self.q
+            m = np.zeros(matrix_size)
+            for i in range(self.r):
+                m[i] = 1
+            np.random.shuffle(m)
+            selection_matrix = m.reshape(self.q, self.p)
+            return selection_matrix
 
-def qks_run(qc, executable, theta):
-    theta_map = {'theta%s'%pos: [theta[pos]] for pos in range(len(theta))}
-    bitstrings = qc.run(executable, memory_map=theta_map)
-    avg_measurements = bitstrings.mean(axis=0)
-    return avg_measurements
+        size = (self.n_episodes, self.q, self.p)
 
+        if self.distribution == "normal":
+            dist = np.random.normal(loc=0, scale=self.scale, size=size)
+        else:
+            raise AttributeError("QKS currently only implemented for normal distributions. Use distribution = 'normal'.")
 
-def logistic_regression(X_train, y_train, X_test, y_test):
-    lr = LogisticRegression(solver='lbfgs')
-    lr.fit(X_train, y_train)
+        selection_matrix = np.array([_create_selection_matrix() for x in range(self.n_episodes)])
+        omega = dist * selection_matrix # matrix chooses which values to keep
 
-    train_acc = lr.score(X_train, y_train)
-    test_acc = lr.score(X_test, y_test)
+        return omega
 
-    print(
-        "accuracy\n----- \n training: {}\n test:     {}"
-          .format(train_acc, test_acc)
-         )
+    def _make_beta(self):
+        """random q-dimensional bias vector"""
+        return np.random.uniform(low=0, high=(2 * np.pi), size=(self.n_episodes, self.q))
 
-    train_preds = lr.predict(X_train)
-    test_preds = lr.predict(X_test)
+    def _get_theta(self, u):
+        """A linear transformation to get our set of random parameters to feed into the quantum circuit
 
-    return train_preds, test_preds
+        The code is written in such a way to maintain the same notation used in the paper.
 
+        u: p-dimensional input vector from the dataset 
+        """
 
-def make_plot(data, target, name, title):
-    plt.figure(figsize=(5, 5))
-    plt.title(title)
-    plt.scatter(data[:,0], data[:,1], s=5, c=target)
-    plt.savefig(name)
+        thetas = []
+        for i in range(u.shape[0]):
+            for e in range(self.n_episodes):
+                thetas.append(self.omega[e].dot(u[i].T) + self.beta[e])
 
-
-def main():
-    np.random.seed(1337)
-
-    n_episodes = 20    # number of episodes the dataset is iterated over
-    sigma2 = 1         # spread of the distribution.
-    n_trials = 1000    # number of shots
-    noisy = False      # use pre-configured noise model
-    q = 2              # number of qubits to use
-    plot = True        # plot graph of the classified datasets
-    train_size = 200   # 200 is default to get the size of the dataset in the paper
-    test_size = 50     # 50 is default to get the size of the dataset in the paper
+        return np.array(thetas)
     
-    img_path = 'figs/'
-    num_cols = n_episodes * q # number of columns after QKS transformation
+    def _build_and_compile(self):
+        """ Creates the quantum circuit and compiles the program into an executable. """
 
-    # make picture frames dataset
-    train, y_train = make_frames(train_size)
-    test, y_test = make_frames(test_size)
+        def _ansatz():
+            """Mimics the circuits from the appendix of the paper, with the exception of
+            the ordering of the CNOT gates (before and after compilation they still do not match).
+            This is probably best just left up to the compiler. 
 
-    p = train.shape[-1] # dimension of input vector
-    r = 1 if p / q < 1 else int(p / q)  # number of elements that are non-zero s.t. r<=p
+            For ease of reading the printed operations, the qubits are looped over several times.
+            """    
+            program = Program()
+            qubits = self.qc.qubits()
+            n_qubits = self.q
+            var = 'theta'
 
-    beta  = get_beta(n_episodes, q)
-    omega = make_omega(n_episodes, p, q, r, sigma2, 0)
+            ro = program.declare('ro', memory_type='BIT', memory_size=n_qubits)
+            thetas = {var + str(qubit): program.declare(var + str(qubit), memory_type = 'REAL') for qubit in qubits}
 
-    train_theta = get_theta(omega, train, beta, n_episodes)
-    test_theta = get_theta(omega, test, beta, n_episodes)
+            sq = int(np.sqrt(n_qubits))
+            lim = n_qubits - sq - 1 
 
-    # make QC, circuits, run and measure
-    qc = make_qc(q, noisy)
-    program = ansatz(qc)
-    program.wrap_in_numshots_loop(shots=n_trials)
-    executable = qc.compile(program)
+            for m in qubits:
+                program += RX(thetas[var + str(m)], m)
 
-    train_QKS = [qks_run(qc, executable, theta) for theta in train_theta]
-    test_QKS = [qks_run(qc, executable, theta) for theta in test_theta]
+            for m in qubits:
+                m_1 = m + 1
+                m_sq = m + sq
+                skip = (m_1) % sq
 
-    # ensure that the shape is correct for regression/classification
-    X_train = np.array(train_QKS).reshape(train.shape[0], num_cols)
-    X_test = np.array(test_QKS).reshape(test.shape[0], num_cols)
+                if m_1 < n_qubits:
+                    if (m_sq >= n_qubits): program += CNOT(m, m_1)
+                    else: program += CNOT(m, m_sq)
 
-    train_preds, test_preds = logistic_regression(X_train, y_train, X_test, y_test)
+                if (m < lim) and (skip != 0): program += CNOT(m, m_1)
 
-    if plot is True:
-        make_plot(train, y_train, img_path + 'training_dataset', 'Training Set')
-        make_plot(test, y_test, img_path + 'test_dataset', 'Test Set')
-        make_plot(train, train_preds, img_path + 'results_experiment_train', 'Experiment Results - Training Dataset')
-        make_plot(test, test_preds, img_path + 'results_experiment_test', 'Experiment Results - Test Dataset') 
+            for m in qubits:
+                program += MEASURE(m, ro[m])
 
+            print("program instructions from pyquil:\n")
+            for instruction in program.instructions:
+                print(instruction) 
+                
+            return program
+        
+        program = _ansatz()
+        program.wrap_in_numshots_loop(shots=self.n_trials)
+        executable = self.qc.compile(program)
+        return executable
 
-if __name__ == "__main__":
-    main()
+    def _qks_run(self, theta):
+        """Runs the QKS transformation through the quantum circuit and returns the average measurements"""
+        theta_map = {'theta%s'%pos: [theta[pos]] for pos in range(len(theta))}
+        bitstrings = self.qc.run(self.executable, memory_map=theta_map)
+        avg_measurements = bitstrings.mean(axis=0)
+        return avg_measurements
+
+    
